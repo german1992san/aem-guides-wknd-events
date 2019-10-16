f9a44e2c80a4d8a0936d722d026ad431eb66fd05
german <german.sanchez@soultech.com.mx>
2019-10-16 15:19:40 -0500

    tuto completo
    Signed-off-by: german <german.sanchez@soultech.com.mx>

---
+++ react-app/src/components/MappedComponents.js
@@ -0,0 +1,5 @@
+ /* Dedicated file to include all React components that map to an AEM component */
+require('./page/Page');
+require('./text/Text');
+require('./image/Image');
+require('./list/List');
